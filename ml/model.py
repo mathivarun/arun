89e94401ef78ed755ccd9e297188a8b145363ee9
mathivarun <mathivarun2006@gmail.com>
2025-02-24 15:56:06 +0530

    machine learning project

---
+++ ml/model.py
@@ -0,0 +1,30 @@
+#import all the dependencies
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+import pickle
+
+#load the csv file
+df=pd.read_csv('iris.csv')
+print(df.head())
+print('----------------------------------------------------------------------')
+print(df.tail())
+
+
+X = df[["Sepal_Length", "Sepal_Width","Petal_Length","Petal_Width"]]
+y= df["Class"]
+                                                                                                
+#select the dataset into train and test
+X_train,X_test,y_train,y_test = train_test_split(X ,y)
+
+
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+classifier = RandomForestClassifier()
+
+classifier.fit(X_train,y_train)
+
+pickle.dump(classifier,open("model.pkl","wb"))
